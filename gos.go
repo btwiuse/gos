93c9086541202d3b1e412674d49664bd67059bf9
navigaid <navigaid@gmail.com>
2017-06-17 15:32:52 +0800

    serve contento in /home/aaron/github/gos and enable listing

--- gos.go
+++ gos.go
@@ -9,11 +9,11 @@ import (
 )
 
 var (
-	dir     = flag.String("dir", "./public", "static dir path")
+	dir     = flag.String("dir", ".", "static dir path")
 	listen  = flag.String("listen", ":8000", "listen port")
 	user    = flag.String("user", "", "basic auth user name")
 	pass    = flag.String("pass", "", "basic auth user password")
-	listDir = flag.Bool("listDir", false, "if list dir files")
+	listDir = flag.Bool("listDir", true, "if list dir files")
 	exts    = flag.String("exts", "", "filtered ext files will be supplied")
 )
 
